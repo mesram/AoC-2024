be1dd7c87395749fe63f6c3c988bc8cb17e3fcfc
Joshua Harding <>
2024-12-02 17:41:11 +1030

    Day 02 complete

---
+++ day02/part2.js
@@ -0,0 +1,38 @@
+function isSafe(report) {
+    let [previous, ...levels] = report
+    let safe = true;
+    let increasing = null;
+    for (const level of levels) {
+        const delta = level - previous;
+        if (increasing !== null && increasing !== delta > 0) {
+            safe = false;
+            break;
+        }
+
+        increasing = delta > 0;
+
+        if (Math.abs(delta) < 1 || Math.abs(delta) > 3) {
+            safe = false;
+            break;
+        }
+
+        previous = level;    
+    }
+
+    return safe;
+}
+
+let safeCount = 0;
+for await (const line of console) {
+    const report = line.split(" ").map(Number);
+    
+    for (let i = 0; i < report.length; i += 1) {
+        let permutation = [...report.slice(0, i), ...report.slice(i + 1)];
+        if (isSafe(permutation)) {
+            safeCount += 1;
+            break;
+        }
+    }
+}
+
+console.log(safeCount);
