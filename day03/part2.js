4e354ce7f8c78df6d513f772eb728bf41c5168d8
Joshua Harding <>
2024-12-03 18:37:34 +1030

    Day 3 JS to be similar in structure to swift code.

--- day03/part2.js
+++ day03/part2.js
@@ -1,18 +1,12 @@
 let total = 0;
-
 let enabled = true;
 for await (const line of console) {
-    const doExpression = "do\\(\\)"
-    const dontExpression = "don't\\(\\)"
-    const mulExpression = "mul\\((\\d{1,3}),(\\d{1,3})\\)"
-    const expression = new RegExp(`${doExpression}|${dontExpression}|${mulExpression}`, "g");
+    const expression = new RegExp(`do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\)`, "g");
     for (const [wholeMatch, num1, num2] of line.matchAll(expression)) {
-        if (wholeMatch === "do()") {
-            enabled = true;
-        } else if (wholeMatch === "don't()") {
-            enabled = false;
-        } else if (enabled) {
-            total += Number(num1) * Number(num2);
+        switch (wholeMatch) {
+            case "do()": enabled = true; continue;
+            case "don't()": enabled = false; continue;
+            default: if (enabled) { total += Number(num1) * Number(num2); }
         }
     }
 }
