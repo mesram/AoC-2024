dd45553cd9bd7a715367a81eade961d192b60e46
Joshua Harding <>
2024-12-03 18:34:28 +1030

    Remove redundant extra capture in Day 3 part 2

--- day03/part2.js
+++ day03/part2.js
@@ -5,8 +5,8 @@ for await (const line of console) {
     const doExpression = "do\\(\\)"
     const dontExpression = "don't\\(\\)"
     const mulExpression = "mul\\((\\d{1,3}),(\\d{1,3})\\)"
-    const expression = new RegExp(`(${doExpression}|${dontExpression}|${mulExpression})`, "g");
-    for (const [wholeMatch, _, num1, num2] of line.matchAll(expression)) {
+    const expression = new RegExp(`${doExpression}|${dontExpression}|${mulExpression}`, "g");
+    for (const [wholeMatch, num1, num2] of line.matchAll(expression)) {
         if (wholeMatch === "do()") {
             enabled = true;
         } else if (wholeMatch === "don't()") {
--- day03/part2.swift
+++ day03/part2.swift
@@ -1,12 +1,12 @@
 var total = 0
 var enabled = true
 while let line = readLine() {
-    let expression = #/(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))/#
+    let expression = #/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/#
     for match in line.matches(of: expression) {
         switch match.output.0 {
             case "do()": enabled = true
             case "don't()": enabled = false
-            default: if (enabled) { total += Int(match.output.2!)! * Int(match.output.3!)! }
+            default: if (enabled) { total += Int(match.output.1!)! * Int(match.output.2!)! }
         }
     }
 }
