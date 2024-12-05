a23dfc378950982e311fc495625187c3b25f3a47
Joshua Harding <>
2024-12-05 19:34:44 +1030

    Make array window function better

--- day04/part1.js
+++ day04/part1.js
@@ -2,14 +2,11 @@ import { arrayWindow2d } from "../utils/arrayWindow";
 
 let rows = [];
 for await (const line of console) {
-    rows.push((line + "...").split(""));
+    rows.push(line.split(""));
 }
 
-let extraLine = ".".repeat(rows[0].length).split("");
-rows.push(extraLine, extraLine, extraLine);
-
 let count = 0;
-for (const w of arrayWindow2d(rows, [4, 4])) {
+for (const w of arrayWindow2d(rows, [4, 4], { overflow: true, emptyValue: "." })) {
     const right = w[0][0] + w[0][1] + w[0][2] + w[0][3]
     const down =  w[0][0] + w[1][0] + w[2][0] + w[3][0]
     const downRight =  w[0][0] + w[1][1] + w[2][2] + w[3][3]
--- utils/arrayWindow.js
+++ utils/arrayWindow.js
@@ -10,10 +10,16 @@ export function arrayWindow1d(source, [columns]) {
     return result;
 }
 
-export function arrayWindow2d(source, [rows, columns]) {
+export function arrayWindow2d(
+    source, 
+    [rows, columns], 
+    { overflow = false, overlap = true, emptyValue } = {}
+) {
+    const height = source.length;
+    const width = source[0].length;
     const result = [];
-    for (let row = 0; row <= source.length - rows; row += 1) {
-        for (let column = 0; column <= source[row].length - columns; column += 1) {
+    for (let row = 0; row <= height - (overflow ? 1 : rows); row += overlap ? 1 : rows) {
+        for (let column = 0; column <= width - (overflow ? 1 : columns); column += overlap ? 1 : columns) {
             const item = []
             result.push(item);
 
@@ -21,7 +27,7 @@ export function arrayWindow2d(source, [rows, columns]) {
                 const item2 = []
                 item.push(item2);
                 for (let j = 0; j < columns; j += 1) {
-                    item2.push(source[row + i][column + j]);
+                    item2.push(source[row + i]?.[column + j] ?? emptyValue);
                 }
             }
         }
