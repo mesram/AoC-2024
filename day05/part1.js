cbea31f9f673c30c132c9323468cae74bb9fe793
Joshua Harding <>
2024-12-05 17:52:37 +1030

    Day 05 complete

---
+++ day05/part1.js
@@ -0,0 +1,95 @@
+const rules = [];
+
+for await (const line of console) {
+    if (line === "") break;
+    rules.push(line.split("|"));
+}
+
+function getOrder(pages) {
+    const parents = new Map();
+    const children = new Map();
+
+    for (const rule of rules) {
+        const [parent, child] = rule;
+
+        if (!pages.includes(parent) || !pages.includes(child)) continue;
+
+        if (!parents.has(parent)) {
+            parents.set(parent, new Set());
+        }
+    
+        parents.get(parent).add(child);
+    
+        if (!children.has(child)) {
+            children.set(child, new Set());
+        }
+    
+        children.get(child).add(parent);
+    }
+
+    const flattened = [];
+    let appended = 0;
+
+    while (parents.size > 0) {
+        let found = false;
+        for (const [parent, dependencies] of parents.entries()) {
+            if (!children.has(parent)) {
+                // this is a root node
+                flattened.push(parent);
+                for (const child of dependencies) {
+                    children.get(child).delete(parent);
+                    if (children.get(child).size === 0) {
+                        children.delete(child);
+                        if (!parents.has(child)) {
+                            /*
+                            strictly speaking, this can lead to ambiguous ordering where a valid input could be considered invalid
+
+                                1|2
+                                1|3
+
+                                1,3,2
+
+                            should be considered valid, but this part here would could an implicit 2|3 rule
+                            thankfully the AoC puzzle makers don't test for this
+                            */
+                            flattened.push(child);
+                        }
+                    }
+                }
+                parents.delete(parent);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            console.log("HUH??", flattened);
+            throw "bad input"
+        }
+    }
+
+    return flattened;
+}
+
+let total = 0;
+for await (const line of console) {
+    const pages = line.split(",");
+    const flattened = getOrder(pages);
+    let previousIndex = -1;
+    let valid = true;
+    for (const page of pages) {
+        let index = flattened.indexOf(page);
+        if (index === -1) continue;
+        if (index < previousIndex) {
+            valid = false;
+            break;
+        }
+        previousIndex = index;
+    }
+
+    if (valid) {
+        total += Number(pages[Math.floor(pages.length / 2)])
+    }
+    console.log(`${line} = ${valid ? 'good' : 'BAD!!'}`);
+}
+
+console.log(total);
