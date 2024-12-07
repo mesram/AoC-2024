34f13e46a45fb8bcd374a249e9b558f0d7653b32
Joshua Harding <>
2024-12-07 16:42:51 +1030

    Day 07 complete

---
+++ day07/part1.js
@@ -0,0 +1,23 @@
+let total = 0;
+for await (const line of console) {
+    console.log(line);
+    const pieces = line.split(": ");
+    const target = Number(pieces[0]);
+    const numbers = pieces[1].split(" ").map((n) => Number(n));
+
+    if (matches(target, numbers)) {
+        total += target;
+    }
+}
+
+console.log(total);
+
+function matches(target, numbers) {
+    if (numbers.length === 1) {
+        return numbers[0] === target;
+    }
+
+    const [left, right, ...remaining] = numbers;
+    return matches(target, [left + right, ...remaining])
+        || matches(target, [left * right, ...remaining]);
+}
