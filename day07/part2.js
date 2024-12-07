961604cd836e707d06a32b05674b7df8bd1ad6de
Joshua Harding <>
2024-12-07 19:42:07 +1030

    remove some optimisations from day07 for better code clarity

--- day07/part2.js
+++ day07/part2.js
@@ -2,24 +2,23 @@ let total = 0;
 for await (const line of console) {
     const pieces = line.split(": ");
     const target = Number(pieces[0]);
-    const numbers = pieces[1].split(" ").map((n) => Number(n));
+    const [first, ...numbers] = pieces[1].split(" ").map((n) => Number(n));
 
-    if (matches(target, numbers, 1, numbers[0])) {
+    if (matches(target, numbers, first)) {
         total += target;
     }
 }
 
 console.log(total);
 
-function matches(target, numbers, i, currentValue) {
-    if (i === numbers.length) {
+function matches(target, numbers, currentValue) {
+    if (numbers.length === 0) {
         return currentValue === target;
     }
 
-    let val = numbers[i]
+    const [val, ...rest] = numbers
 
-    return matches(target, numbers, i + 1, currentValue + val)
-        || matches(target, numbers, i + 1, currentValue * val)
-        || matches(target, numbers, i + 1, Number(`${currentValue}${val}`))
-    ;
+    return matches(target, rest, currentValue + val)
+        || matches(target, rest, currentValue * val)
+        || matches(target, rest, Number(`${currentValue}${val}`));
 }
--- day07/part2.swift
+++ day07/part2.swift
@@ -2,23 +2,23 @@ var total = 0
 while let line = readLine() {
     let pieces = line.split(separator: ": ")
     let target = Int(pieces[0])!
-    let numbers = pieces[1].split(separator: " ").map { Int($0)!}
+    let numbers = pieces[1].split(separator: " ").map { Int($0)! }
 
-    if (matches(target: target, numbers: numbers, i: 1, currentValue: numbers[0])) {
+    if (matches(target, numbers.dropFirst(), numbers.first!)) {
         total += target
     }
 }
 
 print(total)
 
-func matches(target: Int, numbers: [Int], i: Int, currentValue: Int) -> Bool {
-    if (i == numbers.count) {
+func matches(_ target: Int, _ numbers: ArraySlice<Int>, _ currentValue: Int) -> Bool {
+    guard let num = numbers.first else {
         return currentValue == target
     }
 
-    let num = numbers[i]
+    let nextSequence = numbers.dropFirst();
 
-    return matches(target: target, numbers: numbers, i: i + 1, currentValue: currentValue + num)
-        || matches(target: target, numbers: numbers, i: i + 1, currentValue: currentValue * num)
-        || matches(target: target, numbers: numbers, i: i + 1, currentValue: Int("\(currentValue)\(num)")!)
+    return matches(target, nextSequence, currentValue + num)
+        || matches(target, nextSequence, currentValue * num)
+        || matches(target, nextSequence, Int("\(currentValue)\(num)")!)
 }
