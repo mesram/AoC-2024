dd35bfe8ccf9e07cc8d4c618773d18101e5a9f57
Joshua Harding <>
2024-12-10 20:23:25 +1030

    Day 10 complete

---
+++ day10/part2.js
@@ -0,0 +1,32 @@
+let map = [];
+
+for await (const line of console) {
+    map.push(line.split("").map(Number));
+}
+
+let height = map.length;
+let width = map[0].length;
+
+let total = 0;
+for (let row = 0; row < height; row += 1) {
+    for (let column = 0; column < width; column += 1) {
+        if (map[row][column] === 0) {
+            total += getTrailheadEndpoints(row, column).length;
+        }
+    }
+}
+
+console.log(total);
+
+function getTrailheadEndpoints(row, column) {
+    let currentValue = map[row][column];
+    if (currentValue === 9) return [`${row}.${column}`];
+
+    return [
+        [row - 1, column],
+        [row + 1, column],
+        [row, column - 1],
+        [row, column + 1],
+    ].filter(([r, c]) => map[r]?.[c] === currentValue + 1)
+    .flatMap(([r, c]) => getTrailheadEndpoints(r, c));
+}
